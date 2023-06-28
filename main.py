3584cc70c1bc6c05f1a17dc87c732136f8e0e1a3
abi wardani <abiwardani@abis-MacBook-Pro.local>
2023-06-28 11:51:12 +0700

    pwd python

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def generate_password(length=12):
+    """Generate a secure password."""
+    # Menggabungkan karakter huruf besar, huruf kecil, angka, dan simbol
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Mengacak urutan karakter
+    secure_password = ''.join(random.choice(characters) for _ in range(length))
+    return secure_password
+
+# Menghasilkan password dengan panjang 12 karakter
+password = generate_password()
+print("Password Anda:", password)
